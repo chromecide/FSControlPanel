2094601b8a96e928afd5fd6018fe725ac011f225
Justin Pradier <chromecide@chromecide.com>
2012-11-28 23:38:01 +1100

    continued development

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ new FluxNode({
 		{
 			name: 'GroupSecurity',
 			options:{
-				loginTimeout: 10,
+				loginTimeout: 90,
 				method: 'permission',
 				/*store: {
 					databaseName: 'tunnel_auth_test'
@@ -22,9 +22,30 @@ new FluxNode({
 			name: __dirname+'/lib/mixins/ApplicationServer'
 		},
 		{
+			name: __dirname+'/lib/mixins/ApplicationServer/dataInstall.js'
+		},
+		{
 			name: 'websockets'
+		},
+		{
+			name: 'webserver',
+			options:{
+				autoStart: true, 
+				port: 8081
+			}
 		}
 	]
 }, function(thisNode){
-	
+	thisNode.on('GroupSecurity.UserLoggedIn', function(user){
+		console.log('WE HAD A SUCCESS');
+		console.log(arguments);
+		thisNode.emit('GroupSecurity.SendMessage', {
+			user: user.id,
+			topic: 'FluxNode.Mixin',
+			message:{
+				name: 'mixins/GroupSecurity/admin_client',
+				options: ''
+			}
+		});
+	})
 });
